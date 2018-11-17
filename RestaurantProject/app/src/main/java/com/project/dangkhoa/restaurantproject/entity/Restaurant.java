59c1b39c215d03a1bd2c485877cf797d8c762840
Hoàng Đăng Khoa <hoangdangkhoa.m9@gmail.com>
2018-11-17 19:58:30 +0700

    delete old project

--- RestaurantProject/app/src/main/java/com/project/dangkhoa/restaurantproject/entity/Restaurant.java
+++
@@ -1,9 +0,0 @@
-package com.project.dangkhoa.restaurantproject.entity;
-
-public class Restaurant {
-    public String Name;
-    public String Category;
-    public double RatingPoints; // from 0 to 10 stars
-    public String ImageURL;
-
-}
